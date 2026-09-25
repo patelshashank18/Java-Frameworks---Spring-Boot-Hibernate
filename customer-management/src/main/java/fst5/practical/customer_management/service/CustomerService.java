4f9eb85c051f60ef9a4bc4e137f30d1f045e7f5d
Shashank Patel <2002shashank@gmail.com>
2026-09-25 16:02:27 +0530

    Add Customer service

---
+++ customer-management/src/main/java/fst5/practical/customer_management/service/CustomerService.java
@@ -0,0 +1,11 @@
+package fst5.practical.customer_management.service;
+
+import fst5.practical.customer_management.dto.CustomerDto;
+
+/**
+ * Service interface for customer operations.
+ */
+public interface CustomerService
+        extends GenericService<CustomerDto, Long> {
+
+}
