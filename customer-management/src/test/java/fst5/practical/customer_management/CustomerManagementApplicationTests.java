e7e5d2df5ffca5cd29190b3453eefc547c371700
Shashank Patel <2002shashank@gmail.com>
2026-09-25 16:04:45 +0530

    Add customer management tests

---
+++ customer-management/src/test/java/fst5/practical/customer_management/CustomerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package fst5.practical.customer_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
